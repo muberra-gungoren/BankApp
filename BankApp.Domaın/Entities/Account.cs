9c6f56e32307337e980e4fc925616d8f05496616
SenaTasci <tascisena99@gmail.com>
2024-05-29 09:50:17 +0300

    [Feat] Account Class Added and Solution Edıt

---
+++ BankApp.Domaın/Entities/Account.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp.Domaın.Entities
+{
+    public class Account
+    {
+        public Guid Id { get; set; }
+    }
+}
