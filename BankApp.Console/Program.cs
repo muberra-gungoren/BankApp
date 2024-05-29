b3540b5cfccb1e16ec102c5efdff33fd2e00764a
Fatima Ekin <111661799+FatimaEkin@users.noreply.github.com>
2024-05-29 10:54:25 +0300

    [Feat] Add Program.cs

--- BankApp.Console/Program.cs
+++ BankApp.Console/Program.cs
@@ -1,5 +1,15 @@
 ﻿
-public class Program
-{
+using BankApp.Domaın.Services;
 
-}
+
+BankAppService bankAppService = new ();
+string name = Console.ReadLine();
+string surname = Console.ReadLine();
+string identidyNumber = Console.ReadLine();
+string accountNumber = Console.ReadLine();
+string email = Console.ReadLine();
+string password = Console.ReadLine();
+string phoneNumber = Console.ReadLine();
+
+    bankAppService.SignUp( name, surname, identidyNumber, accountNumber,  email, password, phoneNumber);
+  
