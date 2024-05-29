8b41c1fc4c9bdedc0677613648f0eecc5c7861be
SenaTasci <tascisena99@gmail.com>
2024-05-29 09:58:36 +0300

    [Feat] Added Account Class

--- BankApp.Domaın/Entities/Account.cs
+++ BankApp.Domaın/Entities/Account.cs
@@ -9,5 +9,21 @@ namespace BankApp.Domaın.Entities
     public class Account
     {
         public Guid Id { get; set; }
+
+        public string Name { get; set;}
+
+        public string Surname { get; set;}
+
+        public string IdentidyNumber { get; set;}
+
+        public string AccountNumber { get; set;}
+
+        public string Email { get; set;}
+
+        public string Password { get; set;}
+
+        public decimal Balance { get; set;}
+
+        public string PhoneNumber { get; set;}
     }
 }
